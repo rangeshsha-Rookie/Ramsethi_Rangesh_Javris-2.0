f9774fab8d68c1216c90a261975acbedf1ba37b6
Rangesh Gupta <rangeshsha@gmail.com>
2026-04-14 16:49:30 +0530

    Add API handler stubs

---
+++ phishguard-india/api/check-vpa.js
@@ -0,0 +1,3 @@
+module.exports = async function checkVpa(req, res) {
+  res.json({ ok: true, message: "check-vpa stub" });
+};
---
+++ phishguard-india/api/report-fraud.js
@@ -0,0 +1,3 @@
+module.exports = async function reportFraud(req, res) {
+  res.json({ ok: true, message: "report-fraud stub" });
+};
